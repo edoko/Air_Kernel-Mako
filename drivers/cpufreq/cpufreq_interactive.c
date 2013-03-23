d21dd730122fcd439419c3f2c7e07012eb8900b2
Seongmin Park <r_data@naver.com>
2013-03-23 18:23:29 +0900

    Revert "interactive: change permission for boostpulse from root to system"
    This reverts commit 5ba3a01897f2de6500ca4dcd5fe35470d463f12c.

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -34,9 +34,6 @@
 #define CREATE_TRACE_POINTS
 #include <trace/events/cpufreq_interactive.h>
 
-#include <linux/syscalls.h>
-#include <linux/highuid.h>
-
 static int active_count;
 
 struct cpufreq_interactive_cpuinfo {
@@ -124,18 +121,6 @@ struct cpufreq_governor cpufreq_gov_interactive = {
 	.max_transition_latency = 10000000,
 	.owner = THIS_MODULE,
 };
-
-#define	AID_SYSTEM	(1000)
-static void dbs_chown(void)
-{
-	int ret;
-
-	ret =
-	sys_chown("/sys/devices/system/cpu/cpufreq/interactive/boostpulse",
-		low2highuid(AID_SYSTEM), low2highgid(0));
-	if (ret)
-		pr_err("sys_chown: boostpulse error: %d", ret);
-}
 
 static void cpufreq_interactive_timer_resched(
 	struct cpufreq_interactive_cpuinfo *pcpu)
@@ -930,8 +915,6 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 			return -EINVAL;
 
 		mutex_lock(&gov_lock);
-
-		dbs_chown();
 
 		freq_table =
 			cpufreq_frequency_get_table(policy->cpu);
