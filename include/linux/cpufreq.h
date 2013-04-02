ddbe8c1eb24f9fd6342d9cb94d60261e8cdf72c4
Seongmin Park <r_data@naver.com>
2013-04-03 00:24:45 +0900

    Add HYPER governor (Made by Dorimanx) and Ported by ME.
    Signed-off-by: Seongmin Park <r_data@naver.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -381,6 +381,9 @@ extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
 extern struct cpufreq_governor cpufreq_gov_wheatley;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_wheatley)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYPER)
+extern struct cpufreq_governor cpufreq_gov_hyper;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_hyper)
 #endif
 
 
