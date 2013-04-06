a37d380ded64dc6d89755ddb2e425c11e816b407
Seongmin Park <r_data@naver.com>
2013-04-07 00:25:28 +0900

    Add Nightmare and Ktoonservative governors ! (Thanks to Alucard24 and ktoonsez)
    Signed-off-by: Seongmin Park <r_data@naver.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -384,6 +384,9 @@ extern struct cpufreq_governor cpufreq_gov_wheatley;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYPER)
 extern struct cpufreq_governor cpufreq_gov_hyper;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_hyper)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
+extern struct cpufreq_governor cpufreq_gov_nightmare;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_nightmare)
 #endif
 
 
