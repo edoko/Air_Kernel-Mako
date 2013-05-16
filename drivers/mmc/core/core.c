feac94d64af185501468300dbe00c4aec295cfc8
Ken Sumrall <ksumrall@android.com>
2013-05-15 20:13:13 -0700

    mmc: Add tracepoints of mmc block operations
    Add tracepoints to record the start and end of each mmc block
    operation.  This includes read, write, erase, secure erase,
    trim, secure trim1 and secure trim 2, discard and
    sanitize commands.
    
    Change-Id: Ic5d1cbdb9adb940d8b1a2a13c73970023575df50
    Signed-off-by: Ken Sumrall <ksumrall@android.com>
    Signed-off-by: edoko <r_data@naver.com>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -29,6 +29,8 @@
 #include <linux/wakelock.h>
 #include <linux/pm.h>
 
+#include <trace/events/mmc.h>
+
 #include <linux/mmc/card.h>
 #include <linux/mmc/host.h>
 #include <linux/mmc/mmc.h>
@@ -191,6 +193,7 @@ void mmc_request_done(struct mmc_host *host, struct mmc_request *mrq)
 			pr_debug("%s:     %d bytes transferred: %d\n",
 				mmc_hostname(host),
 				mrq->data->bytes_xfered, mrq->data->error);
+			trace_mmc_blk_rw_end(cmd->opcode, cmd->arg, mrq->data);
 		}
 
 		if (mrq->stop) {
@@ -457,8 +460,12 @@ struct mmc_async_req *mmc_start_req(struct mmc_host *host,
 		err = host->areq->err_check(host->card, host->areq);
 	}
 
-	if (!err && areq)
+	if (!err && areq) {
+		trace_mmc_blk_rw_start(areq->mrq->cmd->opcode,
+				       areq->mrq->cmd->arg,
+				       areq->mrq->data);
 		start_err = __mmc_start_req(host, areq->mrq);
+	}
 
 	if (host->areq)
 		mmc_post_req(host, host->areq->mrq, 0);
@@ -1655,7 +1662,12 @@ static int mmc_do_erase(struct mmc_card *card, unsigned int from,
 {
 	struct mmc_command cmd = {0};
 	unsigned int qty = 0;
+	unsigned int fr, nr;
 	int err;
+
+	fr = from;
+	nr = to - from + 1;
+	trace_mmc_blk_erase_start(arg, fr, nr);
 
 	/*
 	 * qty is used to calculate the erase timeout which depends on how many
@@ -1748,6 +1760,8 @@ static int mmc_do_erase(struct mmc_card *card, unsigned int from,
 	} while (!(cmd.resp[0] & R1_READY_FOR_DATA) ||
 		 R1_CURRENT_STATE(cmd.resp[0]) == R1_STATE_PRG);
 out:
+
+	trace_mmc_blk_erase_end(arg, fr, nr);
 	return err;
 }
 
